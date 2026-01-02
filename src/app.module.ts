3d6ede84743d33a6139f2112bf2cdff5cd019cd9
Fabiana <fabiana.asara@gmail.com>
2026-01-02 17:49:09 +0000

    Validation schema with joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,12 +6,20 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CoffeesModule } from './modules/coffee.module';
 import { CoffeeRatingModule } from './modules/coffee-rating.module';
 import { GreetingModule } from './modules/greetings.module';
+import Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
+      // By default schema keys are optional, so we need to mark them as required
+      validationSchema: Joi.object({
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DATABASE: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().default(5432),
+      }),
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
