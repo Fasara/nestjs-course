1862d6716d779a636dc8e74679022df0111d45b0
Fabiana <fabiana.asara@gmail.com>
2026-01-03 12:13:50 +0000

    Custom exception filter

---
+++ src/common/filters/htt-exception/htt-exception.filter.spec.ts
@@ -0,0 +1,7 @@
+import { HttExceptionFilter } from './htt-exception.filter';
+
+describe('HttExceptionFilter', () => {
+  it('should be defined', () => {
+    expect(new HttExceptionFilter()).toBeDefined();
+  });
+});
---
+++ src/common/filters/htt-exception/htt-exception.filter.ts
@@ -0,0 +1,31 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+
+@Catch(HttpException)
+export class HttExceptionFilter<
+  T extends HttpException,
+> implements ExceptionFilter {
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+    const errorMessage =
+      // If the exception response is a string we create an object and put the string inside the message property
+      typeof response === 'string'
+        ? { message: exceptionResponse }
+        : (exceptionResponse as object);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    response.status(status).json({
+      ...errorMessage,
+      timestamp: new Date().toISOString(),
+    });
+  }
+}
