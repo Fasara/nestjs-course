ca08e9b5183dfc3ba8fc10ce1815768c511fbefe
Fabiana <fabiana.asara@gmail.com>
2026-01-03 18:09:02 +0000

    Timeout interceptor

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttExceptionFilter } from './common/filters/htt-exception/htt-exception.filter';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response/wrap-response.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -17,7 +18,10 @@ async function bootstrap() {
     }),
   );
   app.useGlobalFilters(new HttExceptionFilter());
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  app.useGlobalInterceptors(
+    new WrapResponseInterceptor(),
+    new TimeoutInterceptor(),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
